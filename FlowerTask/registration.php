85497ed2242181b2fe5fd13981e4ac8ed0c1288b
fasih <fasihaider1@gmail.com>
2023-01-23 14:14:44 +0500

    First Task

---
+++ FlowerTask/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Fasih_FlowerTask', __DIR__);
---
+++ FlowerTask/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/action/place-order': {
+                'Fasih_FlowerTask/js/order/flower-order-mixin': true
+            },
+        }
+    }
+};
